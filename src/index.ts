0b5ff6f3f686ab6bd16c62e45a18f001f43c6cd9
Gabriel Feitosa <gabriel.perino99@gmail.com>
2021-08-14 21:06:40 -0300

    feat: database config

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,10 @@ import helmet from "helmet";
 import dotenv from "dotenv";
 import path from "path";
 const pathEnv = path.join(process.cwd() + "/.env");
-import App from "./server"
+import "./database/connection";
+import App from "./server";
 dotenv.config({ path: pathEnv });
 
-
-
-
-const app = new App(
-
-
-  [cors(), helmet()],
-   5000
-);
+const app = new App([cors(), helmet()], 5000);
 
 app.listen();
