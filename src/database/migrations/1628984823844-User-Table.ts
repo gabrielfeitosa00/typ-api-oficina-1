782351dc661d130125c5c2102c99f974f4b29427
Gabriel Feitosa <gabriel.perino99@gmail.com>
2021-08-14 21:06:57 -0300

    feat:  user and role migration

---
+++ src/database/migrations/1628984823844-User-Table.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class UserTable1628984823844 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "user",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "name",
+            type: "varchar(45)",
+          },
+          {
+            name: "password",
+            type: "varchar(45)",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user");
+  }
+}
